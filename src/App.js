0f133b867573d3f73367873840caa4a76a061f46
jumendez16 <jumendez702@gmail.com>
2022-08-01 18:12:39 -0500

    coreccion de cambios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-import React from 'react';
-import './App.css'
 
-import Navbar from "./components/navbar/NavBar";
-const App =() =>(
-  <Navbar />
-)
-=======
 import React from "react";
-import { Button } from "antd";
-
 import Navbar from "./components/navbar/NavBar";
->>>>>>> 8b1a6405455fc540b8fc2fa6567e9eb968fa7011
 
 
 const App =() =>(
