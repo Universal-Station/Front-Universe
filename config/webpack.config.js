8b1a6405455fc540b8fc2fa6567e9eb968fa7011
jumendez16 <101608595+jumendez16@users.noreply.github.com>
2022-08-01 17:12:20 -0500

    Add NavBar

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -293,6 +293,11 @@ module.exports = function (webpackEnv) {
       ],
     },
     resolve: {
+      
+      fallback: { 
+        "stream": require.resolve("stream-browserify"),
+        "crypto": require.resolve("crypto-browserify")
+     },
       // This allows you to set a fallback for where webpack should look for modules.
       // We placed these paths second because we want `node_modules` to "win"
       // if there are any conflicts. This matches Node resolution mechanism.
