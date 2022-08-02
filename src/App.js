8ee00682d1f8208cc23478b9be9c3b7311597ef6
jumendez16 <jumendez702@gmail.com>
2022-08-01 21:03:57 -0500

    Create form Register User

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegisterUser from "./components/forms/RegisterUser";
 import Navbar from "./components/navbar/NavBar";
 
 
-const App =() =>(
-  <Navbar />
-)
+function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar/>}/>
+        <Route path="/RegisterUser" element={<RegisterUser/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default App
